1d10a219491c1b9201def2ee2bdd677201520ddb
nadiaissaoui <nadia.issaoui99@gmail.com>
2017-03-11 09:37:37 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://127.0.0.1/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://127.0.0.1/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/camel/www/upload/catalog/');
+define('DIR_SYSTEM', 'C:/camel/www/upload/system/');
+define('DIR_IMAGE', 'C:/camel/www/upload/image/');
+define('DIR_LANGUAGE', 'C:/camel/www/upload/catalog/language/');
+define('DIR_TEMPLATE', 'C:/camel/www/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/camel/www/upload/system/config/');
+define('DIR_CACHE', 'C:/camel/www/upload/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/camel/www/upload/system/storage/download/');
+define('DIR_LOGS', 'C:/camel/www/upload/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/camel/www/upload/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/camel/www/upload/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'admin');
+define('DB_DATABASE', 'store');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
